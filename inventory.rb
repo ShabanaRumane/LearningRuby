0a6b637716b2d2df400aaf4d6ec475705a9afa72
Shabana Rumane <shabana.rumane@shopify.com>
2019-11-07 13:06:39 -0500

    modified inventory to add func to move inventory between locations

--- inventory.rb
+++ inventory.rb
@@ -17,13 +17,37 @@ class Location
             @inventory[prod] +=count
         end
     end
-
+    
     def get_inventory(prod)
         @inventory[prod]
     end
+    
     def show_inventory
-        @inventory.each do |key, value|
-            puts "#{key} - #{value}"
+        @inventory
+        #@inventory.each do |key, value|
+        #    puts "#{key} - #{value}"
+        #end
+        #return nil
+    end
+    
+    def prod_exists?(prod)
+        if @prod_rec[prod] == nil
+            false
+        else
+            true
+        end
+    end
+
+    def move_inventory_to(to_loc,prod,qty)
+        if @prod_rec[prod] != nil and to_loc.prod_exists?(prod) 
+            if @inventory[prod] > qty+2
+                @inventory[prod] -=qty
+                to_loc.add_product_qty(prod,qty)            
+            else 
+                puts "Not enough quantity of #{prod} in #{@location_name} to move"
+            end
+        else
+            puts "*********Product does not exist to move*********" 
         end
     end
 end
@@ -41,30 +65,39 @@ class Product
 
 end
 
+=begin
 p1 = Product.new({:common_name=>"Shirt", :cost_price =>10, :sell_price=>15,:category=>"Garments",:aisle => "L1"})
 #p2 = Product.new("Shirt",10,10,"Garments")
 p2 = Product.new({:common_name=>"Pants", :cost_price =>20, :sell_price=>25,:category=>"Garments",:aisle => "L1"})
 #p2 = Product.new("Shirt",10,10,"Garments")
+=end 
+
 
 l1 = Location.new("basement","myhouse")
 l1.add_prod_rec("Shirt", 10, 15,"Garments")
 l1.add_prod_rec("Pants", 20, 25,"Garments")
 l1.add_prod_rec("Jacket", 18, 23,"Garments")
+l1.add_prod_rec("Cap", 18, 23,"Garments")
 
-puts "#{p1.common_name} belongs to the category #{p1.category} with Cost price to be #{p1.cost_price} that should be sold at #{p1.sell_price} "
-
-
-#l1.add_product_qty(p1,10)
-#l1.add_product_qty(p1,100)
+#puts "#{p1.common_name} belongs to the category #{p1.category} with Cost price to be #{p1.cost_price} that should be sold at #{p1.sell_price} "
 
 l1.add_product_qty("Shirt",10)
 l1.add_product_qty("Shirt",1000)
-l1.add_product_qty("Pant",8)
+l1.add_product_qty("Pants",8)
 l1.add_product_qty("Jacket",222)
 l1.add_product_qty("Jacket",9)
 l1.add_product_qty("Jacket",90)
 l1.add_product_qty("Cap",9)
-#puts l1.get_inventory(p1)
-#puts l1.get_inventory(p2)
-puts "Shirts are #{l1.get_inventory("Shirt")} "
-l1.show_inventory
+puts "i am here"
+puts "Inventory in #{l1.location_name} is: #{l1.show_inventory}"
+l2 = Location.new("warehouse","170 Simonston Blvd")
+l2.add_prod_rec("Shirt", 10, 15,"Garments")
+l2.add_prod_rec("Pants", 20, 25,"Garments")
+l2.add_prod_rec("Jacket", 18, 23,"Garments")
+l2.add_product_qty("Shirt",1000)
+l2.add_product_qty("Pants",8)
+l2.add_product_qty("Jacket",222)
+puts "Inventory in #{l2.location_name} is: #{l2.show_inventory}"
+l1.move_inventory_to(l2,"Shirt",1500)
+puts "Inventory in #{l1.location_name} is: #{l1.show_inventory}"
+puts "Inventory in #{l2.location_name} is: #{l2.show_inventory}"
